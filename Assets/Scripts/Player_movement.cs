c007ef2c4042e15427dafe7e12e476305b6f1a53
SAM LLOYD <SL295211@falmouth.ac.uk>
2023-12-07 13:51:27 +0000

    made the player move using left joystick

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player_movement : MonoBehaviour
 {
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public void Joystic_Movement()
+    {
+       InputActionAsset actions = GetComponent<PlayerInput>().actions;
+       Vector2 movement = actions.FindAction("Movement").ReadValue<Vector2>();
+       GetComponent<Rigidbody2D>().velocity = new Vector2(movement.x * speed, movement.y * speed);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        Joystic_Movement();
     }
 }
