8c85ba3f4f8e91e21fe840859d3625a5025ca33b
Calum Laptop <calum.j.stanley@icloud.com>
2023-12-08 20:21:58 +0000

    Created health behaviour script and added to current entities

---
+++ Assets/Scripts/EntityHealthBehaviour.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the ability for a gameobject to possess health, to heal/take damage or die.
+/// </summary>
+public class EntityHealthBehaviour : MonoBehaviour
+{
+    [SerializeField] private int entityMaxHealth;
+
+    // This is public so that it can be accessed by UI.
+    [HideInInspector] public int entityCurrentHealth;
+
+    private void Start()
+    {
+        entityCurrentHealth = entityMaxHealth;
+    }
+
+    /// <summary>
+    /// Reduces the entity's health by a set amount
+    /// </summary>
+    /// <param name="damageAmount">Amount of damage to apply</param>
+    public void ApplyDamage(int damageAmount)
+    {
+        entityCurrentHealth -= damageAmount;
+
+        print($"{gameObject.name} took {damageAmount} damage, current health: {entityCurrentHealth}");
+
+        // Check to see if the entity has taken lethal damage.
+        // If so, kill it.
+        if (entityCurrentHealth <= 0)
+        {
+            EntityDeath();     
+        }
+        
+    }
+
+    /// <summary>
+    /// Restores missing entity health by a set amount.
+    /// </summary>
+    /// <param name="healAmount">Amount of health to restore</param>
+    public void ApplyHeal(int healAmount)
+    {
+        // We check to see if the heal would exceed the players max health,
+        // If it does, heal them to full health, else just heal the amount.
+
+        if ((entityCurrentHealth += healAmount) > entityMaxHealth)
+        {
+            entityCurrentHealth = entityMaxHealth;
+        }
+        else
+        {
+            entityCurrentHealth += healAmount;
+        }
+
+        print($"Restored {healAmount} health to {gameObject.name}, current health: {entityCurrentHealth}");
+    }
+
+    private void EntityDeath()
+    {
+        if (gameObject.CompareTag("Enemy"))
+        {
+            // Deduct from enemy counter here.
+
+            print("Death");
+
+            Destroy(gameObject);
+        }
+        else if (gameObject.CompareTag("Player"))
+        {
+            // Set player as dead in gamemanager here.
+        }
+    }
+}
