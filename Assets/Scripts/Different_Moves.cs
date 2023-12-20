a0fd9d562b78c12b213f8c9c1b8635085a07d034
SL295211 <Sl295211@falmouth.ac.uk>
2023-12-20 18:36:04 +0000

    added a basic meele attack to the fallen angel.

--- Assets/Scripts/Different_Moves.cs
+++ Assets/Scripts/Different_Moves.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Different_Moves : MonoBehaviour
 {
+    public LayerMask LayersToHit;
     // Start is called before the first frame update
     void Start()
     {
         
     }
     /// <summary>
-    /// a Melee attack that contains a float for y range, int for damage, string for layer to target
+    /// a Melee attack that contains a float for y range, int for damage
     /// </summary>
     /// <param name="Melee_info"></param>
     public void Melee(AnimationEvent Melee_info)
     {
-        
-        LayerMask targetLayer = LayerMask.NameToLayer(Melee_info.stringParameter);
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, new Vector2(transform.localScale.x, Melee_info.floatParameter) ,transform.rotation.z, Vector2.up, Melee_info.floatParameter , targetLayer.value);
-        Debug.Log(hit.collider.gameObject.name);
-        hit.collider.gameObject.GetComponent<EntityHealthBehaviour>().ApplyDamage(Melee_info.intParameter);
+
+        RaycastHit2D[] hit = Physics2D.BoxCastAll(transform.position, new Vector2(transform.localScale.x, Melee_info.floatParameter) ,transform.rotation.z, Vector2.up, Melee_info.floatParameter, LayersToHit);
+        foreach (RaycastHit2D hit2d in hit)
+        {
+            hit2d.collider.gameObject.GetComponent<EntityHealthBehaviour>().ApplyDamage(Melee_info.intParameter);
+        }
 
 
     }
