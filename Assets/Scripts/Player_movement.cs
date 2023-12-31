41a9963cf449909b71451fd974bfcd1271a8ded6
SL295211 <Sl295211@falmouth.ac.uk>
2023-12-31 18:01:48 +0000

    animation controllers for both dante and vigil done!

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -10,11 +10,17 @@ public class Player_movement : MonoBehaviour
     private float delay = -0.05f;
     private Different_Moves moves;
     [SerializeField] private LayerMask GunTargets;
+    private Rigidbody2D rb;
+    private Animator ani;
+    private float time;
+    private bool VelocityCheck;
     // Start is called before the first frame update
     void Start()
     {
         actions = GetComponent<PlayerInput>().actions;
         moves=  GetComponent<Different_Moves>();
+        rb = GetComponent<Rigidbody2D>();   
+        ani = GetComponent<Animator>();
     }
     public void Joystic_Movement()
     {
@@ -22,7 +28,58 @@ public class Player_movement : MonoBehaviour
        Vector2 movement = actions.FindAction("Movement").ReadValue<Vector2>();
        GetComponent<Rigidbody2D>().velocity = new Vector2(movement.x * speed, movement.y * speed);
        float heading  = Mathf.Atan2(movement.x, movement.y);
-       transform.rotation = new Quaternion(0,0, heading *Mathf.Rad2Deg, 0);
+       //transform.rotation = new Quaternion(0,0, heading *Mathf.Rad2Deg, 0);
+    }
+    private void Animation_Controller()
+    {
+        Vector2 movement = actions.FindAction("Movement").ReadValue<Vector2>();
+        float velo = Mathf.Abs(rb.velocity.x + rb.velocity.y);
+        ani.SetFloat("Velocity",velo);
+        if (velo < 0.0001  && !VelocityCheck) 
+        {
+            time = Time.time;
+            VelocityCheck = true;
+            return;
+        }
+        Debug.Log(Mathf.Abs(rb.velocity.y));
+        if (Mathf.Abs(rb.velocity.y) > Mathf.Abs(rb.velocity.x))
+        {
+            Debug.Log(rb.velocity.y);
+            if (rb.velocity.y < 0)
+            {
+                Debug.Log("y>x");
+                ani.SetBool("Y>X", true);
+                ani.SetBool("Positive Y>X change", false);
+            }
+            else
+            {
+                ani.SetBool("Y>X", false);
+                ani.SetBool("Positive Y>X change", true);
+            }
+        }
+        else
+        {
+            if (rb.velocity.x < 0)
+            {
+                Debug.Log("left");
+                ani.SetBool("Negative x", true);
+            }
+            else
+            {
+                Debug.Log("right");
+                ani.SetBool("Negative x", false);
+            }
+            ani.SetBool("Y>X", false);
+            ani.SetBool("Positive Y>X change", false);
+        }
+    }
+    public void Flip_Xaxis()
+    {
+        GetComponent<SpriteRenderer>().flipX = true;
+    }
+    public void UnFlip_Xaxis()
+    {
+        GetComponent<SpriteRenderer>().flipX = false;
     }
     private void Player_Melee()
     {
@@ -33,6 +90,28 @@ public class Player_movement : MonoBehaviour
     private void Player_Shooting()
     {
         moves.Shoot(GunTargets, transform.position, transform.right);
+    }
+    private void IdleCheck()
+    {
+        float velo = Mathf.Abs(rb.velocity.x + rb.velocity.y);
+        Debug.Log("pass 1");
+        if (velo < 0.0001)
+        {
+            Debug.Log("pass 2");
+            if (Time.time - 5 > time)
+            {
+                ani.SetBool("Time passed 5", true);
+                Debug.Log("pass 3");
+            }
+            return;
+        }
+        else
+        {
+            VelocityCheck = false;
+            ani.SetBool("Time passed 5", false);
+            return;
+        }
+        
     }
     // Update is called once per frame
     void Update()
@@ -46,6 +125,11 @@ public class Player_movement : MonoBehaviour
         {
             Player_Shooting();
         }
+        if (VelocityCheck) 
+        {
+            IdleCheck();
+        }
+        Animation_Controller();
         Joystic_Movement();
     }
 }
