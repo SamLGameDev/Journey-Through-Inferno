e34bbef4ac92a4b4e495b3c34035c788b930efc2
Israel <ID298517@falmouth.ac.uk>
2024-01-30 16:31:05 +0000

    Add Magician Arcana

--- Assets/Scripts/Tarot_cards.cs
+++ Assets/Scripts/Tarot_cards.cs
@@ -11,6 +11,7 @@ public class Tarot_cards : MonoBehaviour
     public bool hasStrength = false;
     public bool hasStar = false;
     public bool hasTemperance = false;
+    public bool hasMagician = false;
     
     // Start is called before the first frame update
     void Start()
