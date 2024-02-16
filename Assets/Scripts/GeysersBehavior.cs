a0963ebae177d96cb2c26670ed0d2574250910f1
Israel <ID298517@falmouth.ac.uk>
2024-02-16 22:36:56 +0000

    Added Geysers For Greed Level
    A sprite needs to be made for the gold lava AOE

---
+++ Assets/Scripts/GeysersBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeysersBehavior : MonoBehaviour
+{
+    public float aoeRange;
+    public int aoeDamage;
+    public GameObject aoeEffectPrefab;
+    public float aoeCooldown;
+
+    private float aoeTimer; // Timer tracks cooldown 
+    private GameObject aoeEffect;
+
+    private void Start()
+    { 
+        // Starts the AOE timer
+        aoeTimer = aoeCooldown; 
+    }
+
+    private void Update()
+    {
+        // Updates the AOE timer
+        aoeTimer -= Time.deltaTime;
+
+        // Checks if the AOE timer has reached zero
+        if(aoeTimer <= 0f)
+        {
+            // Instantiates AOE
+            TriggerAOE();
+
+            // Resets the AOE timer
+            aoeTimer = aoeCooldown;
+        }
+    }
+
+    private void TriggerAOE()
+    {
+        // Instantiates AOE prefab and destroys it after a little while 
+        if(aoeEffectPrefab != null) 
+        {
+            aoeEffect = Instantiate(aoeEffectPrefab, transform.position, Quaternion.identity);
+            Destroy(aoeEffect, 0.5f); 
+        }
+
+        // Deal damage to player 
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, aoeRange);
+        foreach(Collider2D collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                // Apply damage to player
+                collider.GetComponent<EntityHealthBehaviour>().ApplyDamage(aoeDamage);
+            }
+        }
+    }
+
+}
