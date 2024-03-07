80c87b609f1463996747662f3a1094a9cdd112da
FRANKIE DE VOS <fd299192@falmouth.ac.uk>
2024-03-07 11:28:29 +0000

    Clone movement done

--- Assets/Scripts/EnemyScripts/Pluto/PlutoBehaviour.cs
+++ Assets/Scripts/EnemyScripts/Pluto/PlutoBehaviour.cs
@@ -9,6 +9,7 @@ public class Pluto_behaviour : MonoBehaviour
     private int[,] places = { { -17, 17, 0, -15, 2, 16 }, { 18, 18, 17, -3, -12, -2 } };
     private float moveInterval = 10f;
     private float moveTimer;
+    private bool hasUsedClone = false;
 
 
     void Start()
@@ -20,6 +21,28 @@ public class Pluto_behaviour : MonoBehaviour
     {
         int rand = Random.Range(0, 6);
         transform.position = new Vector3(places[0, rand], places[1, rand], 0);
+        if (rand >= 4)
+        {
+            transform.GetChild(0).position = new Vector3(places[0, rand - 2], places[1, rand - 2], 0);
+            transform.GetChild(1).position = new Vector3(places[0, rand - 4], places[1, rand - 4], 0);
+        }
+        else if (rand >= 2)
+        {
+            transform.GetChild(0).position = new Vector3(places[0, rand + 2], places[1, rand + 2], 0);
+            transform.GetChild(1).position = new Vector3(places[0, rand - 2], places[1, rand - 2], 0);
+        }
+        else
+        {
+            transform.GetChild(0).position = new Vector3(places[0, rand + 2], places[1, rand + 2], 0);
+            transform.GetChild(1).position = new Vector3(places[0, rand + 4], places[1, rand + 4], 0);
+        }
+    }
+
+    void Clone()
+    {
+        MovePlaces();
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
     }
 
 
@@ -30,6 +53,12 @@ public class Pluto_behaviour : MonoBehaviour
         {
             MovePlaces();
             moveTimer -= moveInterval;
+        }
+
+        if (GetComponent<EntityHealthBehaviour>().entityCurrentHealth <= 10)
+        {
+            Clone();
+            hasUsedClone = true;
         }
     }
 }
