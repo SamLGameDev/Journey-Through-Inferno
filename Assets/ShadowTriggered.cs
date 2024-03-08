f00c3455dd2b19b9580a0e702f9613d3e5c163eb
SL295211 <145340757+CantThinkOfAUserNameAllTaken@users.noreply.github.com>
2024-03-08 17:34:37 +0000

    icicles done!

---
+++ Assets/ShadowTriggered.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowTriggered : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GetComponentInParent<SpikeFall>().notHit = false;
+    }
+
+}
