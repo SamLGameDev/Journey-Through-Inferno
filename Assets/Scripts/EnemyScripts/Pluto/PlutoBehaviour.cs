93a60718dbe313852bc774c09ee29cc90d6e5884
FRANKIE DE VOS <fd299192@falmouth.ac.uk>
2024-03-12 13:34:42 +0000

    added cerberus script

--- Assets/Scripts/EnemyScripts/Pluto/PlutoBehaviour.cs
+++ Assets/Scripts/EnemyScripts/Pluto/PlutoBehaviour.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 
 public class Pluto_behaviour : MonoBehaviour
 {
-    
+    [SerializeField] private Pluto stats;
     private int[,] places = { { -17, -16, -7, 7, 20, 18 }, { 13, -3, -21, -7, -3, 13 } };
-    private float moveInterval = 10f;
+    public float moveInterval = 10f;
     private float moveTimer;
     private bool hasUsedClone = false;
-
 
     void Start()
     {
@@ -19,23 +18,22 @@ public class Pluto_behaviour : MonoBehaviour
 
     private void MovePlaces()
     {
-        int rand = Random.Range(0, 6);
-        transform.position = new Vector3(places[0, rand], places[1, rand], 0);
-        if (rand >= 4)
+        int rand1 = Random.Range(0, 6);
+        transform.position = new Vector3(places[0, rand1], places[1, rand1], 0);
+        
+        int rand2= Random.Range(0, 6);
+        while (rand1 == rand2)
         {
-            transform.GetChild(0).position = new Vector3(places[0, rand - 2], places[1, rand - 2], 0);
-            transform.GetChild(1).position = new Vector3(places[0, rand - 4], places[1, rand - 4], 0);
+            rand2 = Random.Range(0, 6);
         }
-        else if (rand >= 2)
+        int rand3 = Random.Range(0, 6);
+        while ((rand1 == rand3) || (rand2 == rand3))
         {
-            transform.GetChild(0).position = new Vector3(places[0, rand + 2], places[1, rand + 2], 0);
-            transform.GetChild(1).position = new Vector3(places[0, rand - 2], places[1, rand - 2], 0);
+            rand3 = Random.Range(0, 6);
         }
-        else
-        {
-            transform.GetChild(0).position = new Vector3(places[0, rand + 2], places[1, rand + 2], 0);
-            transform.GetChild(1).position = new Vector3(places[0, rand + 4], places[1, rand + 4], 0);
-        }
+
+        transform.GetChild(0).position = new Vector3(places[0, rand2], places[1, rand2], 0);
+        transform.GetChild(1).position = new Vector3(places[0, rand3], places[1, rand3], 0);
     }
 
     void Clone()
