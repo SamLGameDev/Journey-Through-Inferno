5b892a55bce37a7c329708eb73370630bb9f1dc7
CALUM STANLEY <CS303830@falmouth.ac.uk>
2024-04-09 11:54:58 +0100

    Fixed walking sfx

--- Assets/AudioCaller.cs
+++ Assets/AudioCaller.cs
@@ -6,9 +6,29 @@ public class AudioCaller : MonoBehaviour
 {
     private AudioManager manager;
 
+    [SerializeField] private float stepTime;
+
+    private float time;
+
+    [SerializeField] private string walkAnimName;
+
     private void Start()
     {
         manager = AudioManager.instance;
+    }
+
+    private void Update()
+    {
+        if (GetComponent<Animator>().GetFloat("Velocity") > 0)
+        {
+            if (time < 0)
+            {
+                manager.PlaySound(walkAnimName);
+                time = stepTime;
+            }
+            print(time);
+            time -= Time.deltaTime;
+        }
     }
 
     public void CallSound(string name)
