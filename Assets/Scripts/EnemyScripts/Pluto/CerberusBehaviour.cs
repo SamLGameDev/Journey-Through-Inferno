88cd611efe0f2d22a7d117d5ada5929b3d3a7a91
FRANKIE DE VOS <fd299192@falmouth.ac.uk>
2024-04-11 20:46:13 +0100

    debug pluto

--- Assets/Scripts/EnemyScripts/Pluto/CerberusBehaviour.cs
+++ Assets/Scripts/EnemyScripts/Pluto/CerberusBehaviour.cs
@@ -7,18 +7,14 @@ public class CerberusBehaviour : MonoBehaviour
     [SerializeField] private Pluto stats;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print(collision.gameObject);
-
         if (collision.CompareTag("Player"))
         { 
             collision.GetComponent<EntityHealthBehaviour>().ApplyDamage(stats.damage);
-            print(collision.gameObject);
             Destroy(gameObject); 
         }
 
         if (collision.CompareTag("Wall"))   
         { 
-            print(collision.gameObject);
             Destroy(gameObject); 
         }
     }
