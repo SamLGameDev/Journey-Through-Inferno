19b2f640b54d52f5a5fe7ddc268b7f4bdd57e557
JOSHUA WESBROOM <JW273249@falmouth.ac.uk>
2024-04-30 11:49:54 +0100

    Game Balancing start.

--- Assets/Scripts/Scriptable-Objects/Player.cs
+++ Assets/Scripts/Scriptable-Objects/Player.cs
@@ -102,13 +102,15 @@ public class Player : BasicAttributes
     public GameObject confusionBullet;
     public float ConfusionCooldown;
     public PlayerState currentState;
+
+    public List<TarotCards> Permenant_Cards;
     public override void Reset()
     {
         chariotSpeed.value = 0;
         gunCooldownModifier.value = 0;
         swordDamageModifier.value = 0;
         bulletDamageModifier.value = 0;
-        tarotCards.Clear();
+        tarotCards = Permenant_Cards;
         extraLives = 0;
         cardDropChance = 0;
         maxHealth = 15;
