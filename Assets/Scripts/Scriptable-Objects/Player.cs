5c8bdc3a6bad2c3163bf01f74a7e348f6701bd4e
SAM LLOYD <SL295211@falmouth.ac.uk>
2024-05-10 12:34:30 +0100

    UPDATED projectile nsize

--- Assets/Scripts/Scriptable-Objects/Player.cs
+++ Assets/Scripts/Scriptable-Objects/Player.cs
@@ -119,7 +119,7 @@ public class Player : BasicAttributes
         criticalHitChance.value = 0;
         damageReduction = 0;
         armour = 0;
-        projectilesize = new Vector3(3,3,1);
+        projectilesize = new Vector3(2,2,1);
         spreadShotNumber.value = 0;
         cooldownReduction.value = 0;
         droppableCards.Clear();
